d64c81797a89b70862aa447956116655140e3ff3
dimon <bratiki@inbox.ru>
2024-01-17 16:14:30 +0300

    add_preview_image_for_service

--- car_service/apiservice/models.py
+++ car_service/apiservice/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+def service_preview_directory_path(instance: "Service", filename: str) -> str:
+    return "services/service_{name}/preview/{filename}".format(
+        name=instance.name,
+        filename=filename,
+    )
+
+
 class Service(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     archived = models.BooleanField(default=False)
+    preview = models.ImageField(null=True, blank=False, upload_to=service_preview_directory_path)
